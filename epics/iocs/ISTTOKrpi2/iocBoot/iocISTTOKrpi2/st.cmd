9bd8504ec2753d5b9cce135acc11e7103872e8da
bernardo carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2021-05-11 11:45:16 +0100

    added autosave for alarm limits

--- epics/iocs/ISTTOKrpi2/iocBoot/iocISTTOKrpi2/st.cmd
+++ epics/iocs/ISTTOKrpi2/iocBoot/iocISTTOKrpi2/st.cmd
@@ -5,6 +5,7 @@
 
 < envPaths
 epicsEnvSet( "STREAM_PROTOCOL_PATH", "$(TOP)/db" )
+epicsEnvSet( "SAVE_DIR", "$(TOP)/iocBoot/$(IOC)" )
 
 cd "${TOP}"
 
@@ -30,9 +31,23 @@ dbLoadRecords("db/ISTTOKpfeiffer.db","P=ISTTOK:,R=vacuum:,bus=RS0")
 #var streamError 1
 #var streamDebug 1
 #streamSetLogfile("stream_logfile.txt")
+
+#save_restoreSet_status_prefix("$(IOC):")
+save_restoreSet_status_prefix("ISTTOK:")
+set_requestfile_path("$(SAVE_DIR)")
+set_savefile_path("$(SAVE_DIR)/save")
+save_restoreSet_NumSeqFiles(3)
+save_restoreSet_SeqPeriodInSeconds(600)
+set_pass0_restoreFile("$(IOC).sav")
+set_pass1_restoreFile("$(IOC).sav")
+#dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=ISTTOK:")
  
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
+
+# Create request file and start periodic 'save’
+# makeAutosaveFileFromDbInfo("$(SAVE_DIR)/$(IOC).req", "autosaveFields")
+create_monitor_set("$(IOC).req", 30)
 
 ## Start any sequence programs
 #seq sncxxx,"user=pi"
