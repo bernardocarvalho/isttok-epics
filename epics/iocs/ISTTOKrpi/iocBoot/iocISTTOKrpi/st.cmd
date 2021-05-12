9007221515a7881515c5523daa3f1215369262eb
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2021-05-12 11:04:00 +0100

    drvI2C working ?

--- epics/iocs/ISTTOKrpi/iocBoot/iocISTTOKrpi/st.cmd
+++ epics/iocs/ISTTOKrpi/iocBoot/iocISTTOKrpi/st.cmd
@@ -21,7 +21,7 @@ drvAsynI2CConfigure( "I2C", "/dev/i2c-1", 1 )
 dbLoadRecords("db/ISTTOKstates.db","P=ISTTOK:,R=central:")
 dbLoadRecords("db/ISTTOKpcf8574.db","P=ISTTOK:,R=central:,BUS=I2C")
 dbLoadRecords("db/ISTTOKpcf8591.db","P=ISTTOK:,R=central:,BUS=I2C")
-dbLoadRecords("db/ISTTOKtda8444.db","P=ISTTOK:,R=central:,BUS=I2C")
+#dbLoadRecords("db/ISTTOKtda8444.db","P=ISTTOK:,R=central:,BUS=I2C")
 dbLoadRecords("db/ISTTOKmachineControl.db","P=ISTTOK:,R=central:")
 
 ## Load Serial drivers
@@ -44,7 +44,7 @@ drvAsynIPPortConfigure("L0","193.136.136.3:25",0,1,0)
 
 dbLoadRecords("db/sendmail.db", "P=ISTTOK:,PORT=L0,R=central:,L=0,A=0")
 
-# Uncomment to Debug STREAM serial activity
+# Uncomment to Debug STREAMDEVICE serial activity
 #var streamError 1
 #var streamDebug 1
 streamSetLogfile("logfile.txt")
