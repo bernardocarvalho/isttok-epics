443eb8857cc55647e7004d263589c509169f957e
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2023-03-08 17:06:02 +0000

    added bob displays
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- epics/css/ArchiveConfigTool/launch-epics-archive.sh
+++ epics/css/ArchiveConfigTool/launch-epics-archive.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #
-#JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 #JAVA_HOME=/opt/java/jdk/jdk1.8.0_231
 JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 PATH=$JAVA_HOME/bin:$PATH
-#cd /home/oper/CSS-Archive/archive-engine-4.1.0
-cd /home/oper/CSS-Archive/archive-engine-4.6.6
-#/home/esther/EPICS/archive-engine-4.1.0
+#cd /home/oper/CSS-Archive/archive-engine-4.6.6
+cd /home/oper/CSS-Archive/archive-engine
 #./ArchiveEngine -pluginCustomization isttok.ini -engine isttokDB -port 4812&
 ./archive-engine.sh -engine isttokDB -port 4812 -settings isttok.ini
 
